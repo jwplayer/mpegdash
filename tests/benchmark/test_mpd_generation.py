5efd4be6fb4829628de19931a149d78083d59f2b
Nuno Pinge <nuno@pinge.org>
2016-02-28 17:33:15 -0500

    added initial mpd generation benchmarks

---
+++ tests/benchmark/test_mpd_generation.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import time
+import pytest
+
+from dashifest import MediaPresentationDescription, Period
+
+
+@pytest.mark.benchmark(
+    group='MPD Generation',
+    min_time=0.1,
+    max_time=2,
+    min_rounds=5,
+    timer=time.time,
+    disable_gc=True,
+    warmup=False
+)
+def test_mpd_with_1_period(benchmark):
+    @benchmark
+    def result():
+        mpd = MediaPresentationDescription(profile='urn:mpeg:dash:profile:isoff-on-demand:2011', minimum_buffer_time=1)
+        mpd.add_period(Period(start=0, duration=10))
+        return mpd.to_xml()
+
+    assert result is not None
+
+
+@pytest.mark.benchmark(
+    group='MPD Generation',
+    min_time=0.1,
+    max_time=2,
+    min_rounds=5,
+    timer=time.time,
+    disable_gc=True,
+    warmup=False
+)
+def test_mpd_with_10_periods(benchmark):
+    @benchmark
+    def result():
+        mpd = MediaPresentationDescription(profile='urn:mpeg:dash:profile:isoff-on-demand:2011', minimum_buffer_time=1)
+        [mpd.add_period(Period(start=0, duration=10)) for _ in range(0, 10)]
+        return mpd.to_xml()
+
+    assert result is not None
+
+
+@pytest.mark.benchmark(
+    group='MPD Generation',
+    min_time=0.1,
+    max_time=2,
+    min_rounds=5,
+    timer=time.time,
+    disable_gc=True,
+    warmup=False
+)
+def test_mpd_with_100_periods(benchmark):
+    @benchmark
+    def result():
+        mpd = MediaPresentationDescription(profile='urn:mpeg:dash:profile:isoff-on-demand:2011', minimum_buffer_time=1)
+        [mpd.add_period(Period(start=0, duration=10)) for _ in range(0, 100)]
+        return mpd.to_xml()
+
+    assert result is not None
