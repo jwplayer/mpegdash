ad49cd6a1b52f8a9a52caaeedd85a959b2ba1ff8
Nuno Pinge <nuno@pinge.org>
2016-03-14 23:44:00 -0400

    fixed on-demand profile unit test :bento:

--- tests/dash/profiles/urn:mpeg:dash:profile:isoff-on-demand:2011/mpd/test_mpd_for_on_demand_profile.py
+++ tests/dash/profiles/urn:mpeg:dash:profile:isoff-on-demand:2011/mpd/test_mpd_for_on_demand_profile.py
@@ -15,6 +15,6 @@ class PeriodOnDemandProfileTest(unittest.TestCase):
     """
 
     def test_mpd_type_attribute(self):
-        mpd = MPD(profile='urn:mpeg:dash:profile:isoff-on-demand:2011', minimum_buffer_time=1)
+        mpd = MPD(profiles='urn:mpeg:dash:profile:isoff-on-demand:2011', min_buffer_time=1)
         xml = etree.fromstring(mpd.to_xml())
         assert 'type' in xml.attrib and xml.attrib.get('type') == 'static'
