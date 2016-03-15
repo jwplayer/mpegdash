16e7f4c7ff2d1d6c013d989bacaf4bd7dd0abef6
Nuno Pinge <nuno@pinge.org>
2016-03-14 23:21:30 -0400

    clean up for DASHModel, updated unit test

--- mpegdash/__init__.py
+++ mpegdash/__init__.py
@@ -2,50 +2,22 @@
 
 from lxml import etree
 
+from mpegdash.serialization import XMLSerializer
 
-class DASHModel(object):
 
-    AVAILABLE_DASH_PROFILES = [
-        'urn:mpeg:dash:profile:full:2011',
-        'urn:mpeg:dash:profile:isoff-on-demand:2011',
-        'urn:mpeg:dash:profile:isoff-live:2011',
-        'urn:mpeg:dash:profile:isoff-main:2011',
-        'urn:mpeg:dash:profile:mp2t-main:2011',
-        'urn:mpeg:dash:profile:mp2t-simple:2011',
-    ]
+class DASHModel(XMLSerializer):
 
     PROFILE_RULES = {}
 
-    def __init__(self, profile=None):
-        if profile is not None and profile not in DASHModel.AVAILABLE_DASH_PROFILES:
-            raise TypeError("DASHModel: profile '{}' is invalid".format(str(profile)))
-        self.profile = profile or DASHModel.AVAILABLE_DASH_PROFILES[0]
-        self.element = None
+    def __init__(self, **kwargs):
+        super(DASHModel, self).__init__(**kwargs)
+        self._attributes = {}
 
-    def element(self):
-        return self.__lxml__().element
+    def set_attribute(self, attribute, value):
+        self._attributes[attribute] = value
+        self._xml_set(attribute, value)
 
-    def __lxml__(self):
-        """
-
-        This function is responsible for initializing the lxml Element instance if it doesn't exist (in this case we can
-        emulate a lazy loading behavior which could be beneficial in situations like map reduce.
-        Any profile rules are also processed here
-
-        @rtype: DASHModel
-        @return: the instance itself
-
-        """
-        if self.element is None and 'TAG' in dir(self.__class__):
-            self.element = etree.Element(self.__class__.TAG)
-        [rule(self) for rule in self.__class__.PROFILE_RULES.get(None, [])]
-        [rule(self) for rule in self.__class__.PROFILE_RULES.get(self.profile, [])]
-        return self
-
-    def to_xml(self, encoding='UTF-8'):
-        return self.__lxml__().__str__(encoding=encoding)
-
-    def __str__(self, encoding='UTF-8'):
-        if self.element is not None:
-            return etree.tostring(self.element, xml_declaration=True, encoding=encoding)
-        return super(DASHModel, self).__str__()
+    def to_xml(self, xml_declaration=False, encoding='UTF-8', pretty_print=False):
+        if 'apply_profile_rules' in dir(self):
+            self.apply_profile_rules()
+        return super(DASHModel, self).to_xml(xml_declaration=True, encoding=encoding, pretty_print=pretty_print)
--- tests/dash/data_model/test_dash_model.py
+++ tests/dash/data_model/test_dash_model.py
@@ -4,44 +4,29 @@ import unittest
 
 from mpegdash import DASHModel
 
+DEFAULT_DASH_PROFILE = 'urn:mpeg:dash:profile:isoff-on-demand:2011'
+
+
+class Concrete(DASHModel):
+
+    TAG = 'MPD'
+
 
 class DASHModelTest(unittest.TestCase):
 
-    def test_dash_model_class(self):
-        assert 'TAG' not in dir(DASHModel)
-        assert 'AVAILABLE_DASH_PROFILES' in dir(DASHModel)
-
-    def test_dash_model_constructor_with_degenerate_args(self):
-        self.assertRaises(TypeError, DASHModel, profile='')
-        self.assertRaises(TypeError, DASHModel, profile=u'')
-        self.assertRaises(TypeError, DASHModel, profile=0)
-        self.assertRaises(TypeError, DASHModel, profile=0.0)
-        self.assertRaises(TypeError, DASHModel, profile=[])
-        self.assertRaises(TypeError, DASHModel, profile={})
+    def test_dash_model_initialization(self):
+        self.assertRaises(AttributeError, DASHModel)
 
     def test_dash_model_properties(self):
-        model = DASHModel()
-        assert 'profile' in dir(model)
-        assert 'element' in dir(model)
+        model = Concrete()
+        assert '_attributes' in dir(model)
 
-    def test_dash_model_default_profile(self):
-        model = DASHModel()
-        assert model.profile == 'urn:mpeg:dash:profile:full:2011'
+    def test_dash_model_methods(self):
+        model = Concrete()
+        assert 'set_attribute' in dir(model)
 
-    def test_dash_model_profiles(self):
-        dash_on_demand_profile = 'urn:mpeg:dash:profile:isoff-on-demand:2011'
-        assert DASHModel(profile=dash_on_demand_profile).profile == dash_on_demand_profile
-
-    def test_dash_model_lxml_representation(self):
-        model = DASHModel()
-        assert '__lxml__' in dir(model)
-        assert model.__lxml__() is model
-
-    def test_dash_model_str_representation(self):
-        model = DASHModel()
-        assert '__str__' in dir(model)
-        assert 'mpegdash.DASHModel' in model.__str__()
-        model.__str__(encoding='UTF-8')
-        model.__str__(encoding='ASCII')
-        # TODO check if there's performance impact when checking the encoding in advance
-        # self.assertRaises(LookupError, model.__str__, encoding='UTF-0')
+    def test_dash_model_xml_serialization(self):
+        model = Concrete()
+        assert 'to_xml' in dir(model)
+        assert model.to_xml().startswith("<?xml ")
+        assert model.to_xml().endswith("<MPD/>")
