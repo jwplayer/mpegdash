adbc110ebcf3053d928fd517bcfaabb196c08cd8
Nuno Pinge <nuno@pinge.org>
2016-03-14 21:27:12 -0400

    added first working iteration of a MPD XML serializer :ramen:

---
+++ mpegdash/serialization.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+from datetime import timedelta
+
+from lxml import etree
+import isodate
+
+
+class Serializer(object):
+
+    pass
+
+
+class XMLSerializer(Serializer):
+
+    ATTRIBUTES_TYPES = {
+        'MPD': {
+            'minBufferTime': 'xs:duration',
+            'mediaPresentationDuration': 'xs:duration'
+        },
+        'Period': {
+            'start': 'xs:duration',
+            'duration': 'xs:duration'
+        },
+        'AdaptationSet': {
+            'bitstreamSwitching': 'xs:boolean',
+            'subsegmentAlignment': 'xs:boolean'
+        }
+    }
+
+    def __init__(self, tag=None):
+        self._xml_initialize_element(tag or self.__class__.TAG)
+
+    def to_xml(self, xml_declaration=False, encoding='UTF-8', pretty_print=False):
+        if self._xml_element is not None:
+            return etree.tostring(self._xml_element, xml_declaration=xml_declaration, encoding=encoding,
+                                  pretty_print=pretty_print)
+        raise ValueError('XMLSerializer: to_xml(): element is None')
+
+    def _xml_initialize_element(self, tag=None, nsmap=None, attributes=None):
+        if tag is None:
+            raise TypeError("XMLSerializer: _xml_initialize_element(): tag can't be None")
+        if nsmap is None:
+            nsmap = {}
+        if attributes is None:
+            attributes = {}
+        self._xml_element = etree.Element(tag, nsmap=nsmap, attrib=attributes)
+
+    def _xml_append_element(self, element):
+        self._xml_element.append(element)
+
+    def _xml_set(self, attribute, value):
+        if value is None:
+            if attribute in self._xml_element.attrib:
+                return self._xml_element.attrib.pop(attribute)
+        else:
+            if attribute in XMLSerializer.ATTRIBUTES_TYPES.get(self._xml_element.tag, {}):
+                if XMLSerializer.ATTRIBUTES_TYPES[self._xml_element.tag][attribute] == 'xs:duration':
+                    value = self._xml_to_duration(value)
+                elif XMLSerializer.ATTRIBUTES_TYPES[self._xml_element.tag][attribute] == 'xs:boolean':
+                    value = self._xml_to_bool(value)
+            if not isinstance(value, basestring):
+                value = str(value)
+            self._xml_element.set(attribute, value)
+
+    def _xml_to_duration(self, value):
+        if not isinstance(value, int) and not isinstance(value, float):
+            raise TypeError('XMLSerializer: _xml_to_duration(): value is not int')
+        return isodate.duration_isoformat(timedelta(seconds=value))
+
+    def _xml_to_bool(self, value):
+        if not isinstance(value, bool):
+            raise TypeError('XMLSerializer: _xml_to_bool(): value is not bool')
+        return 'true' if value else 'false'
